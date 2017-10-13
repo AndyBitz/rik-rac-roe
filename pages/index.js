8172de673fb0544cf66ae4963347f85d479754bb
Andy Bitz <artzbitz13+git@gmail.com>
2017-10-13 20:39:58 +0200

    More styles for desktop

--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,25 @@ import Button from '../components/button'
 export default () => (
   <Layout>
     <H style={{ textAlign: 'center' }}>Fik-Fak-Foe</H>
-    <P style={{ textAlign: 'center' }}>Tik Tak Toe for the really bored.</P>
+    <P style={{ textAlign: 'center' }}>
+      Tik Tak Toe for the really bored.<br />
+      <span style={{ fontSize: '.9em' }}>Or challange seekers.</span>
+    </P>
     <Center>
       <Button>
         <Link href="/games/fikfakfoe"><a>play local</a></Link>
       </Button>
     </Center>
+    <style jsx global>
+    {`
+      main {
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+      }
+    `}
+    </style>
   </Layout>
 )
 
