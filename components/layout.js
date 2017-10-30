d251a87c00e3e03e10b41dbed711d9e8415921e8
Andy Bitz <artzbitz13+git@gmail.com>
2017-10-30 12:29:56 +0100

    Added gtag, not working

--- components/layout.js
+++ components/layout.js
@@ -34,6 +34,14 @@ export default ({ children, title="Rik-Rac-Roe" }) => (
       <meta name="twitter:creator" content="@andybitz_" />
       <meta name="twitter:url" content="https://rik-rac-roe.now.sh" />
 
+      <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5WKT69Z');`}} />
+      <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5WKT69Z"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`}} />
+
       <title>{ title }</title>
     </Head>
     { children }
